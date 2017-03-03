5dca6df659b5120c313b2154c6b9ef3f7f431429
Tu Huiyan <tuhuiyan@chaintor.com>
2017-03-03 13:59:05 +0800

    test

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -31,6 +31,7 @@ type SimpleChaincode struct {
 // Main
 // ============================================================================================================================
 func main() {
+	//sdfsdf
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
